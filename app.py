afd7cad0d5c6990289546fd204c6fb9a603b0683
aliblackk <131438173+aliblackk@users.noreply.github.com>
2024-11-18 20:21:38 +0500

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ class CustomCNN(nn.Module):
         x = self.fc2(x)
         return x
 
-file_id = "12VFHcS62ZCyz9ChLUO2cl3azdcgs9X_u"
+file_id = "1GIw3SrrV-UoQFeQYePNEbdA5TmnZRd8-"
 destination = "custom_cnn_model1.pth"
 
 gdrive_url = f"https://drive.google.com/uc?id={file_id}"
